a20596e3fd1bded4e4a5bca34aa2055f77d6b067
carlh <cghinkle427@gmail.com>
2022-01-26 03:50:16 -0500

    Initial Commit

---
+++ AudioDBExplorer/AudioDBExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioDBExplorerApp.swift
+//  AudioDBExplorer
+//
+//  Created by Carl Hinkle on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct AudioDBExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioDBExplorer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AudioDBExplorer
+//
+//  Created by Carl Hinkle on 1/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
